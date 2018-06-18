bc0a60b9f48e1050a4546eb13b5f2cdd2d58393a
SJ_ZHAO <zhaoshj27@163.com>
2018-06-18 17:38:50 +0800

    first update

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -21,22 +21,68 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
+    /**
+    TODO:
     * predict the state
-  */
+    */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
+    /**
+    TODO:
     * update the state by using Kalman Filter equations
-  */
+    */
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd S = H_ * PHt + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
+    /**
+    TODO:
     * update the state by using Extended Kalman Filter equations
-  */
+    */
+    double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+    // if rho is very small, set it to 0.0001 to avoid division by 0 in computing rho_dot
+    if(rho < 0.000001) {
+        rho = 0.000001;
+    }
+    double phi = atan2(x_(1), x_(0));
+    double rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
+    VectorXd h = VectorXd(3); // h(x_)
+    h << rho, phi, rho_dot;
+
+    VectorXd y = z - h;
+    // normalize the angle between -pi to pi
+    while(y(1) > M_PI){
+        y(1) -= 2*M_PI;
+    }
+
+    while(y(1) < -M_PI){
+        y(1) += 2*M_PI;
+    }
+    MatrixXd Ht = H_.transpose();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd S = H_ * PHt + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
